e5389ae56691c69b513047a1413610a3dedc9ea6
catalinavillamil <32912890+catalinavillamil@users.noreply.github.com>
2021-03-27 14:52:37 -0500

    adding R files ok

---
+++ R/recons_fd.R
@@ -0,0 +1,17 @@
+recons_fd = function(X){
+    
+  if (inherits(X,"scores_pred")) {
+    vari=X$vari
+    a=scores(X)
+    mean_coef = X$SFD[[vari]]$fpca$meanfd$coefs
+    nr=nrow(a)
+    mean_coef = matrix(rep(mean_coef,nr),ncol = nr)
+    coef = (X$SFD[[vari]]$fpca$harmonics$coef %*% t(a)) + mean_coef
+    result = fd(coef, X$SFD[[vari]]$fpca$harmonics$basis)
+  }else{
+    stop("Wrong class scores_pred")
+  }
+  return(result)
+  
+}
+
